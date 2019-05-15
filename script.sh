ec6cd3aa6e0a718a2b1c2977e3f818ed7a654a3d
anassolanki <anassolanki330@gmail.com>
2019-05-15 18:09:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp * vagrant@127.0.0.1:/home/vagrant/MyApp
-ssh vagrant@127.0.0.1
+#scp * vagrant@127.0.0.1:/home/vagrant/MyApp
+#ssh vagrant@127.0.0.1
 #tar -xvf copy.tar /home/vagrant/MyApp/
-cd MyApp
+cd /home/vagrant/MyApp
 pm2 start index.js
