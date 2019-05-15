ed9e67acfc4bc7a53e121efab461425e3d6cec13
anassolanki <anassolanki330@gmail.com>
2019-05-15 16:21:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 tar -cvf app.tar /var/lib/jenkins/workspace/FirstProject/
-cp app.tar /home/vagrant
-cd /home/vagrant
+cp app.tar /home/vagrant/
+cd /home/vagrant/
 tar -xvf app.tar
+cd var/lib/jenkins/workspace/FisrtProject/
 pm2 start index.js
