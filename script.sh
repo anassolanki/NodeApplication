c923cc74f375bd61a71a5cfd2b56aef26069c793
anassolanki <anassolanki330@gmail.com>
2019-05-15 18:27:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 #scp * vagrant@127.0.0.1:/home/vagrant/MyApp
 #ssh vagrant@127.0.0.1
 #tar -xvf copy.tar /home/vagrant/MyApp/
-sudo cp /var/lib/jenkins/workspace/FirstProject/ /home/vagrant/MyApp
+#sudo cp /var/lib/jenkins/workspace/FirstProject/ /home/vagrant/MyApp
 cd /home/vagrant/MyApp
 pm2 start index.js
