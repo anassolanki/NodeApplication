de56143fb01b57a3b23c9cce6ccdebca2e28c98c
anassolanki <anassolanki330@gmail.com>
2019-05-15 16:38:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-chmod -R 777 *
-tar -cvf app.tar *
+#chmod -R 777 *
+tar -cvf /home/vagrant/app.tar *
 chmod -R 777 app.tar
 #chown -R vagrant:vagrant app.tar
-sudo cp app.tar /home/vagrant/
+#sudo cp app.tar /home/vagrant/
 cd /home/vagrant/
 tar -xvf app.tar
 cd var/lib/jenkins/workspace/FisrtProject/
