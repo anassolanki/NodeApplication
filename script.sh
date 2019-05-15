3e8765e3b5f87bc59f10b0eed6b50ca4dd2dff37
anassolanki <anassolanki330@gmail.com>
2019-05-15 17:03:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #chmod -R 777 *
-tar -cvf /home/vagrant/app.tar *
-chmod -R 777 app.tar
+#tar -cvf /home/vagrant/app.tar *
+#chmod -R 777 app.tar
 #chown -R vagrant:vagrant app.tar
-cp app.tar /home/vagrant/
+#cp app.tar /home/vagrant/
 cd /home/vagrant/
-chmod -R 755 /home/vagrant
-chmod -R 755 app.tar
-tar -xvf app.tar
-cd var/lib/jenkins/workspace/FisrtProject/
+#chmod -R 755 /home/vagrant
+#chmod -R 755 app.tar
+#tar -xvf app.tar
+#cd var/lib/jenkins/workspace/FisrtProject/
 pm2 start index.js
